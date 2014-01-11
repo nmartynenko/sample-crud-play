d56c39876a3d8ca6092470a9061efaa68d9eb62e
Nickolay Martynenko <n.martynenko@aimprosoft.com>
2014-01-11 22:33:00 +0200

    - finish error handler processor without actual error handlers

--- scala-style/app/mapper/ErrorHandlerProcessor.scala
+++ scala-style/app/mapper/ErrorHandlerProcessor.scala
@@ -4,6 +4,10 @@ import play.api.mvc.Results._
 import play.api.mvc._
 import scala.concurrent.Future
 
+trait ErrorHandler {
+  def handleError(ex: Throwable, request: RequestHeader): Option[SimpleResult]
+}
+
 trait ErrorHandlerProcessor {
 
   val exactMatch: Boolean = false
@@ -11,18 +15,35 @@ trait ErrorHandlerProcessor {
   private val exceptionHandlers: Map[Class[_ <: Throwable], ErrorHandler] = Map()
 
   def handleError(ex: Throwable, request: RequestHeader): Option[Future[SimpleResult]] = {
-    def findMatch(exClass: Class[_ <: Throwable]): Option[ErrorHandler] = {
-      None
+    def findMatch(matchClass: Class[_ <: Throwable]): Option[ErrorHandler] = {
+      exceptionHandlers.get(matchClass) match {
+        case ret @ Some(_) =>
+          ret
+        case None if !exactMatch =>
+          None
+        case _ =>
+          matchClass.getSuperclass match {
+            case sp if classOf[Throwable].isAssignableFrom(sp) =>
+              findMatch(sp.asInstanceOf[Class[Throwable]])
+            case _ =>
+              None
+          }
+      }
     }
 
-    val exClass = ex.getClass
+    val handlerOption = findMatch(ex.getClass)
 
-    //todo
-    None
+    handlerOption match {
+      case Some(handler) =>
+        handler.handleError(ex, request) match {
+          case Some(result) =>
+            Some(Future.successful(result))
+          case _ =>
+            None
+        }
+      case _ =>
+        None
+    }
   }
 
-}
-
-trait ErrorHandler {
-  def handleError(ex: Throwable, request: RequestHeader): Option[SimpleResult]
 }
